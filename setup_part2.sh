eda5061932ff9d68c3e8aee1e82ddd594ecbdcb5
Ubuntu <ubuntu@ip-172-31-1-209.eu-west-1.compute.internal>
2019-09-17 12:31:03 +0000

    changed traefik to alpine

--- setup_part2.sh
+++ setup_part2.sh
@@ -4,3 +4,5 @@ docker network create web
 touch /home/ubuntu/dopeflix/traefik/acme.json
 chmod 600 /home/ubuntu/dopeflix/traefik/acme.json
 mkdir -p /home/ubuntu/media
+mkdir -p /home/ubuntu/dopeflix/jellyfin/config
+mkdir -p /home/ubuntu/dopeflix/jellyfin/cache
